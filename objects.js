8d495111cc6726dfd8c7f670e75f348d8b9c7354
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-01 16:11:17 +0530

    type

--- objects.js
+++ objects.js
@@ -15,3 +15,26 @@ console.log(a[c])
 
 delete a.city;
 console.log(a)
+
+console.log('------------until phase 1---------------')
+
+b=a //pass by reference
+
+console.log(b)
+b.place='Bangalore'
+console.log(b)
+console.log(a)
+console.log("--------------------until phase 2-------------")
+c={sno:a.sno,name:a.name,place:a.place,skill:a.skills}
+
+console.log(c)
+c.place="Mumbai"
+console.log(c)
+console.log(a)
+
+console.log("--------------------until phase 3-------------------")
+
+for(let x in c)
+   console.log(x,'  ',c[x])
+
+   
