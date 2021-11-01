98cdae428c09734243aca33a0024377b06869f84
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-01 15:30:10 +0530

    global-commit

---
+++ index.js
@@ -0,0 +1,14 @@
+a=89
+b="Raj"
+
+console.log("Hello World")
+console.log(window)
+//console.log(this)
+
+// window is the global object in the Browser
+// global is the global object in node js
+
+//every single variable you create in the js page will go to global object
+
+// Window object is not available in javascript that runs on node js
+
