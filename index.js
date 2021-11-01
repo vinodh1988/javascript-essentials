729a237d33293caa79585c6e8d95d859a9ccb02f
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-01 15:44:10 +0530

    type

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-a=89
-b="Raj"
+a=89.99
+b='Raj'
+c=[12,53,'raj','Harry',true,353.4]  //collection of objects
 
-console.log("Hello World")
-console.log(window)
+console.log("Hello 'this is' World")
+//console.log(window)
 //console.log(this)
 
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(true))
+console.log(typeof({sno:1,name:"Rajan"}))
+console.log(typeof(c))
 // window is the global object in the Browser
 // global is the global object in node js
 
