3b12dac6c1719f6a539fd090fdf22c105eb591b9
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-01 16:36:41 +0530

    array-commit

---
+++ arrays.js
@@ -0,0 +1,15 @@
+a=[3,53,534,2534,57]
+//b=a
+b=[...a]
+console.log(a)
+console.log(b)
+b[-10]='Rajan'
+b[1000]="Jackson"
+b['Jack']="India"
+console.log(b)
+console.log(b[-10])
+console.log("------------------------------------")
+for(let x in b) 
+ console.log(x)
+
+ console.log(a)
