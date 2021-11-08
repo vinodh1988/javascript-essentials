81e8c285275c17e533f4898a500df76082e63c1f
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-08 17:11:05 +0530

    typescript-commit

---
+++ Tyepscript/first.js
@@ -0,0 +1,16 @@
+var a = 20;
+console.log(a);
+var b = "Raj";
+console.log(b);
+var c = "Rahul";
+/*
+string
+number
+boolean
+*/
+function fun(a, b) {
+    console.log(a, b);
+}
+fun("Rahul", 124);
+//fun(124,"Rohan")
+fun("Rahul", 245);
---
+++ Tyepscript/first.ts
@@ -0,0 +1,17 @@
+var a=20
+console.log(a)
+var b="Raj"
+console.log(b)
+var c:string ="Rahul"
+/*
+string
+number
+boolean
+*/
+function fun(a:string,b:number):void{
+    console.log(a,b)
+}
+
+fun("Rahul",124)
+//fun(124,"Rohan")
+fun("Rahul",245)
